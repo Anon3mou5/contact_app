2d5ef0c548dcd493376ddc6e2ee9a084031a8f6f
Anon3mou5 <sourabhraobharadwaj@gmail.com>
2021-06-29 22:45:07 +0530

    added model and using sqlite

--- assignment/models.py
+++ assignment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.TextField(primary_key=True)
+    name = models.TextField(default=None)
+    phone_number = models.IntegerField(max_length=10)
+    address = models.TextField()
+
+    def __init__(self, *initial_data, **kwargs):
+        for dictionary in initial_data:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
